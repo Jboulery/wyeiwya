3ef972000acc73d65f8f75156ed83be766f68786
RaphAllier <35372253+RaphAllier@users.noreply.github.com>
2018-02-16 16:32:30 +0100

    Updated all scripts
    Tooltip fix for geomap
    geomap script finished
    completed style.css with style from ranking and geomap
    to do :
    - correct tooltip for ranking
    - adjust map size (div size for ranking/map ?)
    - correct ranking so it fits the div
    
    Co-Authored-By: valentincrr <valentincrr@users.noreply.github.com>

--- scripts/geomap.js
+++ scripts/geomap.js
@@ -1,17 +1,21 @@
-function GeoInit(ByCountry, CritInit, context){
+function GeoInit(ByCountry, CritInit, field,width){
 		
+
+		var context = field.append("svg")
+				.attr("width","100%")
+				.attr("height","150%")
+
+
 		var criteria = CritInit;
 		var countries
 	    // Define svg canvas dimensions
-	    var width = 962,
-	        height = 502;
 
 	    // ranks is the country ordered by the criteria
 		var ranks; 
 	    // define projection
 	    var projection = d3.geoMercator()
-	        .scale(153)
-	        .translate([width/2,height/1.5]);
+	        .scale(80)
+	        .translate([width/3,width/6]);
 
 	    // Create svg canvas
 	    // var svg = context.append("svg")
@@ -23,7 +27,7 @@ function GeoInit(ByCountry, CritInit, context){
 	        .projection(projection);
 
 	    // define tooltip variable
-	    var tooltip = d3.select("#map")
+	    var tooltip = field
 	         .append("div")
 	         .attr("class", "tooltip hidden");
 
@@ -88,7 +92,6 @@ function GeoInit(ByCountry, CritInit, context){
 	      var flag =0;
 	      ByCountry.forEach(function(country){
       		    if (country.key == d.properties.name){
-      		    	console.log(criteria);
           			value = country.means[criteria].toPrecision(3);
           			flag = 1;
           		}
@@ -108,7 +111,7 @@ function GeoInit(ByCountry, CritInit, context){
 	    function GeoUpdate(NewCrit){
 
 	    	criteria = NewCrit;
-	    	console.log("Update with :" + criteria);
+	    	console.log("Update with " + criteria);
 
 		   	// Redefine color scale for countries based on criteria
 		    var colorMin = d3.min(ByCountry, function(d) { return d.means[criteria]; } );
@@ -129,7 +132,7 @@ function GeoInit(ByCountry, CritInit, context){
 		    // draw countries
 			countries
 			  .transition()
-			  .duration(1000)
+			  .duration(4000)
 		      .style("fill",function(geomap){
 		      	var coloring;
 		      	ByCountry.forEach(function(country){
