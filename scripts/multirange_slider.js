556d14724d95671b90709ba2ed881bb9c125c11a
Jboulery <bouleryj@gmail.com>
2018-02-24 00:32:55 +0100

    Creation of ticks for the multirange slider

--- scripts/multirange_slider.js
+++ scripts/multirange_slider.js
@@ -1,7 +1,7 @@
 function sliderInit(){
 	var init = 0
 	var width = 900;
-	var height = 50;
+	var height = 70;
 	var pad = 50, radius = 20, radius2 = 10;
 	var color = d3.scaleOrdinal(d3.schemeCategory10);
 	 
@@ -40,15 +40,27 @@ function sliderInit(){
 					hue(d3.event.x); 
 				}));
 
+	var ticks = slider.insert("g", ".track-overlay")
+	    .attr("class", "ticks")
+	    .attr("transform", "translate(0," + 25 + ")")
+
+	var ticks_labels = ['Energy', 'Fat', 'S. Fat', 'Cholesterol', 'Carbohydrates', 'Sugars', 'Fiber', 'Proteins', 'Salt', 'Vitamin A', 'Vitamin D', 'Calcium', 'Iron'];
+
 	for (var i = 0; i < 13; i++){
 		slider.insert('rect', '.track-overlay')
 			.attr('class', 'handle')
 			.attr('id', 'rect_' + i)
 			.attr('x', width * i / 13)
 			.attr('width', x.range()[1] / 13 + 2)
-			.attr('height', height)
+			.attr('height', 2*height / 3)
 			.attr('transform', 'translate(0, ' + (-height)/2 + ')')
 			.attr('fill', color(i));
+
+		ticks.append("text")
+			.attr('id', 'tick_' + i)
+		    .attr("x", width * i / 13 + width / 13 / 2)
+		    .attr("text-anchor", "middle")
+		    .text(ticks_labels[i]);
 	};
 
 	function hue(h) {
@@ -61,19 +73,27 @@ function sliderInit(){
 				var rect_id_nb = parseInt(rect_id.split('_')[1]);
 				prev_rect_id = 'rect_' + (rect_id_nb - 1);
 				next_rect_id = 'rect_' + (rect_id_nb + 1);
+				tick_id = 'tick_' + rect_id_nb;
+				prev_tick_id = 'tick_' + (rect_id_nb - 1);
 			}
 		});
 		var handle = d3.select('#' + rect_id);
 		var prev_handle = d3.select('#' + prev_rect_id);
 		var next_handle = d3.select('#' + next_rect_id);
+		var tick = d3.select('#' + tick_id);
+		var prev_tick = d3.select('#' + prev_tick_id);
 
 		if(rect_id === 'rect_12'){
 			var new_handle_x_value = x(x.invert(h));
 			var prev_handle_x = parseFloat(prev_handle.attr('x'));
+			var handle_width = Math.abs(width - new_handle_x_value);
+			var prev_handle_width = Math.abs(new_handle_x_value - prev_handle_x);
 
 			handle.attr("x", new_handle_x_value);
-			prev_handle.attr('width', Math.abs(new_handle_x_value - prev_handle_x));
-			handle.attr('width', Math.abs(width - new_handle_x_value));
+			prev_handle.attr('width', prev_handle_width);
+			handle.attr('width', handle_width);
+			tick.attr('x', new_handle_x_value + handle_width / 2);
+			prev_tick.attr('x', new_handle_x_value - prev_handle_width / 2);
 		}
 		else if (rect_id === 'rect_0') {
 			console.log('Les extremites sont fixes !');
@@ -82,10 +102,14 @@ function sliderInit(){
 			var new_handle_x_value = x(x.invert(h));
 			var prev_handle_x = parseFloat(prev_handle.attr('x'));
 			var next_handle_x = parseFloat(next_handle.attr('x'));
+			var handle_width = Math.abs(next_handle_x - new_handle_x_value);
+			var prev_handle_width = Math.abs(new_handle_x_value - prev_handle_x);
 
 			handle.attr("x", new_handle_x_value);
-			prev_handle.attr('width', Math.abs(new_handle_x_value - prev_handle_x));
-			handle.attr('width', Math.abs(next_handle_x - new_handle_x_value));
+			prev_handle.attr('width', prev_handle_width);
+			handle.attr('width', handle_width);
+			tick.attr('x', new_handle_x_value + handle_width / 2);
+			prev_tick.attr('x', new_handle_x_value - prev_handle_width / 2);
 		}
 	}
 };
