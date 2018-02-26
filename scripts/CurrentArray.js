05b54ac5401c13686a878f98baa9e6130dfb98ef
RaphAllier <35372253+RaphAllier@users.noreply.github.com>
2018-02-26 17:08:37 +0100

    Updated by country

--- scripts/CurrentArray.js
+++ scripts/CurrentArray.js
@@ -4,7 +4,7 @@ function UpdateByCountry(ByCountry,proportions){
 	ByCountry.forEach(function(country){
 		var value = 0;
 		for (var i =0; i< 13; i++) {
-			value += proportions[i]*country.means[i]
+			value += proportions[i]*country.normalized[i]
 		}
 		country.criteria = value;
 
